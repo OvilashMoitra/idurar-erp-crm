29b99a6130940e2f4778bee377cdc0f95430fd6a
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-09-16 03:13:32 +0600

    twst

--- server.js
+++ server.js
@@ -33,3 +33,4 @@ app.set('port', process.env.PORT || 8888);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → On PORT : ${server.address().port}`);
 });
+console.log('test');
